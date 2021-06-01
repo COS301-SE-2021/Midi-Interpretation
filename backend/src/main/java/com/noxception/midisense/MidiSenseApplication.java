f624807f371bac72d871e14c6f7e80b7d0f7dc12
Adrian-Rae-19004029 <u19004029@tuks.co.za>
2021-06-01 09:19:02 +0200

    Initial Commit
    Added the Spring and React frameworks necessary for the front and back ends.

---
+++ backend/src/main/java/com/noxception/midisense/MidiSenseApplication.java
@@ -0,0 +1,13 @@
+package com.noxception.midisense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidiSenseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MidiSenseApplication.class, args);
+    }
+
+}
