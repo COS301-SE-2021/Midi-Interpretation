b13dfc1891e9005fd665f194ad32e16dde8be5bb
Adrian-Rae-19004029 <u19004029@tuks.co.za>
2021-06-04 00:36:00 +0200

    Crucial Fix for Swagger

--- backend/src/main/java/com/noxception/midisense/MidiSenseApplication.java
+++ backend/src/main/java/com/noxception/midisense/MidiSenseApplication.java
@@ -2,8 +2,10 @@ package com.noxception.midisense;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class MidiSenseApplication {
 
     public static void main(String[] args) {
--- backend/src/main/java/com/noxception/midisense/interpreter/InterpreterController.java
+++ backend/src/main/java/com/noxception/midisense/interpreter/InterpreterController.java
@@ -1,4 +1,15 @@
 package com.noxception.midisense.interpreter;
 
-public class InterpreterController {
+import com.noxception.midisense.api.InterpreterApi;
+import com.noxception.midisense.models.UploadFileRequest;
+import com.noxception.midisense.models.UploadFileResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InterpreterController implements InterpreterApi {
+    @Override
+    public ResponseEntity<UploadFileResponse> uploadFile(UploadFileRequest body) {
+        return null;
+    }
 }
