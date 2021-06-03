1f23ab9e078dcb6b2a6547e03ece67e8e38a69fa
Hendro Smit <smit.gamedev@gmail.com>
2021-06-03 22:33:41 +0200

    Front end first prototype
    The first implementation of our front end display using React

---
+++ backend/src/app/views/dashboard/Analytics.jsx
@@ -0,0 +1,37 @@
+import React, { Component, Fragment } from "react";
+import { Grid, Card } from "@material-ui/core";
+
+import DoughnutChart from "../charts/echarts/Doughnut";
+
+import ModifiedAreaChart from "./shared/ModifiedAreaChart";
+import StatCards from "./shared/StatCards";
+import TableCard from "./shared/TableCard";
+import RowCards from "./shared/RowCards";
+import StatCards2 from "./shared/StatCards2";
+import UpgradeCard from "./shared/UpgradeCard";
+import Campaigns from "./shared/Campaigns";
+import { withStyles } from "@material-ui/styles";
+
+class Dashboard1 extends Component {
+  state = {};
+
+  render() {
+    let { theme } = this.props;
+
+    return (
+      <Fragment>
+        <div className="pb-24 pt-7 px-8 bg-primary">
+          <div className="card-title capitalize text-white mb-4 text-white-secondary">
+            MIDI File Upload
+          </div>
+        </div>
+
+        <div className="analytics m-sm-30 mt--18">
+            Test Text
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+export default withStyles({}, { withTheme: true })(Dashboard1);
