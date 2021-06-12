18a8b3f919e32dd79aba8e322451ea0c94d61e6a
Adrian-Rae-19004029 <u19004029@tuks.co.za>
2021-06-12 21:37:27 +0200

    Updated Configuration framework

---
+++ backend/src/main/java/com/noxception/midisense/config/MIDISenseConfig.java
@@ -0,0 +1,7 @@
+package com.noxception.midisense.config;
+
+public class MIDISenseConfig {
+    public static final String CROSS_ORIGIN = "*";
+    public static final String FILE_FORMAT = ".mid";
+    public static final String FILE_SYSTEM_PATH = "src/main/java/com/noxception/midisense/midiPool/";
+}
