c0446a9b382174d6bf491c940f3e83fc43dbd029
u18094024 <u18094024@tuks.co.za>
2021-06-17 15:26:31 +0200

    added DisplayController skeleton functions

--- backend/src/main/java/com/noxception/midisense/display/DisplayController.java
+++ backend/src/main/java/com/noxception/midisense/display/DisplayController.java
@@ -1,4 +1,20 @@
 package com.noxception.midisense.display;
 
-public class DisplayController {
+import com.noxception.midisense.api.DisplayApi;
+import com.noxception.midisense.models.DisplayMusicMetadataRequest;
+import com.noxception.midisense.models.DisplayMusicMetadataResponse;
+import com.noxception.midisense.models.DisplayNoteMetadataRequest;
+import com.noxception.midisense.models.DisplayNoteMetadataResponse;
+import org.springframework.http.ResponseEntity;
+
+public class DisplayController implements DisplayApi {
+    @Override
+    public ResponseEntity<DisplayMusicMetadataResponse> displayMusicMetadata(DisplayMusicMetadataRequest body) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<DisplayNoteMetadataResponse> displayNoteMetadata(DisplayNoteMetadataRequest body) {
+        return null;
+    }
 }
