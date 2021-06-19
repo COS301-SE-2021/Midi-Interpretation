d3fd5df30ed3003a798f2e7282b51a7a876548cc
Rea <70331859+u18179802@users.noreply.github.com>
2021-06-19 14:50:27 +0200

    Added processFile controller skeleton

--- backend/src/main/java/com/noxception/midisense/interpreter/InterpreterController.java
+++ backend/src/main/java/com/noxception/midisense/interpreter/InterpreterController.java
@@ -84,6 +84,11 @@ public class InterpreterController implements InterpreterApi {
         return null;
     }
 
+    @Override
+    public ResponseEntity<InterpreterProcessFileResponse> processFIle(InterpreterProcessFileRequest body) {
+        return null;
+    }
+
     // HELPER METHODS BELOW THIS LINE
     //----------------------------------------------------------------------------------------------------
     private byte[] intArrayToByteArray(List<Integer> list)
