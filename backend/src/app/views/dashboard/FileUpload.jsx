71aff62de522fcd11ad2f726d1a085b8705896a2
Adrian-Rae-19004029 <u19004029@tuks.co.za>
2021-06-20 22:06:34 +0200

    Minor frontend Adjustment

--- backend/src/app/views/dashboard/FileUpload.jsx
+++ backend/src/app/views/dashboard/FileUpload.jsx
@@ -2,37 +2,195 @@ import React, {Component, Fragment, useMemo} from "react";
 import {Grid, Card, Button, Fab, Icon} from "@material-ui/core";
 import {Breadcrumb, SimpleCard} from "matx";
 import Dropzone, {useDropzone} from 'react-dropzone'
+import Avatar from '@material-ui/core/Avatar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 
 import { withStyles } from "@material-ui/styles";
 import {makeStyles} from "@material-ui/core/styles";
+import {Text} from "victory";
 
 class FileUpload extends Component {
-  state = {};
 
-    constructor(props) {
-        super(props);
-        this.onDrop = (files) => {
-            this.setState({files})
-        };
-        this.state = {
-            files: []
-        };
-    }
+  constructor(props){
+      super(props);
+      this.onDrop = (files) => {
+          this.fileUpload.uploadButtonText = "UploadFile"
+          this.fileUpload.isSet = true
+          this.fileUpload.files = files
+      };
+      this.fileUpload = {
+          files: [],
+          isSet: false,
+          uploadButtonText: "Please upload a file to continue.",
+          fileDesignator: "ca09ae60-d3df-43b6-95a6-233b5f81ea2c"
+      };
+      this.display = {
+          pieceMetadata: null,
+          trackInfo: null,
+          trackMetadata: null,
+          trackOverview: null,
+          trackIndex: 0
+      }
+
+      this.uploadFile = () => {
+          //get the file
+          const file = this.fileUpload.files[0];
+          console.log(file);
+          const formData = new FormData();
+
+          // Update the formData object
+          formData.append(
+              "myFile",
+              file,
+              file.name
+          );
+
+          const request = new Request("http://localhost:8080/interpreter/uploadFile", {
+              method: 'POST',
+              body: formData,
+              headers: new Headers({
+                  'accept': '*/*',
+                  'Content-Type': 'multipart/form-data'
+              })
+          });
+          fetch(request)
+              .then(res=>res.json())
+              .then((res)=>{
+                  console.log(res)
+              }, (error)=>{
+                  console.log(error)
+              })
+      }
+
+      this.processFile = () => {
+          const request = new Request("http://localhost:8080/interpreter/processFile", {
+              method: 'POST',
+              body: JSON.stringify({
+                  "fileDesignator": this.fileUpload.fileDesignator
+              }),
+              headers: new Headers({
+                  'accept': 'application/json',
+                  'Content-Type': 'application/json'
+              })
+          });
+          fetch(request)
+              .then(res=>res.json())
+              .then((res)=>{
+                  console.log(res)
+              }, (error)=>{
+                  console.log(error)
+              })
+      }
+
+      this.getPieceMetadata = () => {
+          const request = new Request("http://localhost:8080/display/getPieceMetadata", {
+              method: 'POST',
+              body: JSON.stringify({
+                  "fileDesignator": this.fileUpload.fileDesignator
+              }),
+              headers: new Headers({
+                  'accept': 'application/json',
+                  'Content-Type': 'application/json'
+              })
+          });
+          fetch(request)
+              .then(res=>res.json())
+              .then((res)=>{
+                  this.display.pieceMetadata = res
+              }, (error)=>{
+                  console.log(error)
+              })
+      }
+
+      this.getTrackInfo = () => {
+          const request = new Request("http://localhost:8080/display/getTrackInfo", {
+              method: 'POST',
+              body: JSON.stringify({
+                  "fileDesignator": this.fileUpload.fileDesignator
+              }),
+              headers: new Headers({
+                  'accept': 'application/json',
+                  'Content-Type': 'application/json'
+              })
+          });
+          fetch(request)
+              .then(res=>res.json())
+              .then((res)=>{
+                  this.display.trackInfo = res
+              }, (error)=>{
+                  console.log(error)
+              })
+      }
+
+      this.getTrackMetadata = () => {
+          const request = new Request("http://localhost:8080/display/getTrackMetadata", {
+              method: 'POST',
+              body: JSON.stringify({
+                  "fileDesignator": this.fileUpload.fileDesignator,
+                  "trackIndex": this.display.trackIndex
+              }),
+              headers: new Headers({
+                  'accept': 'application/json',
+                  'Content-Type': 'application/json'
+              })
+          });
+          fetch(request)
+              .then(res=>res.json())
+              .then((res)=>{
+                  this.display.trackMetadata = res
+              }, (error)=>{
+                  console.log(error)
+              })
+      }
+
+      this.getTrackOverview = () => {
+          const request = new Request("http://localhost:8080/display/getTrackOverview", {
+              method: 'POST',
+              body: JSON.stringify({
+                  "fileDesignator": this.fileUpload.fileDesignator,
+                  "trackIndex": this.display.trackIndex
+              }),
+              headers: new Headers({
+                  'accept': 'application/json',
+                  'Content-Type': 'application/json'
+              })
+          });
+          fetch(request)
+              .then(res=>res.json())
+              .then((res)=>{
+                  this.display.trackOverview = res
+              }, (error)=>{
+                  console.log(error)
+              })
+      }
+  }
+
+
+
 
   render() {
       const classes = makeStyles;
       let { theme } = this.props;
 
-      const files = this.state.files.map(file => (
+      const files = this.fileUpload.files.map(file => (
           <li key={file.name}>
               {file.name} - {file.size} bytes
           </li>
       ));
 
-
-
       return (
-          <div className="m-sm-30">
+
+
+
+          <div className="m-sm-30" >
               <div className="mb-sm-30">
                   <Breadcrumb
                       routeSegments={[
@@ -54,28 +212,39 @@ class FileUpload extends Component {
                           </section>
                       )}
                   </Dropzone>
+                  <Button disabled={!this.fileUpload.isSet} color="secondary" onClick={this.uploadFile}>
+                      {this.fileUpload.uploadButtonText}
+                  </Button>
               </SimpleCard>
-              <div className="py-3" />
+              <SimpleCard title="Upload File">
+                  <Button onClick={this.processFile}> Process Your File</Button>
+              </SimpleCard>
+
+              <SimpleCard title="Get Piece Metadata">
+                  <div>{JSON.stringify(this.display.pieceMetadata)}</div>
+                  <Button onClick={this.getPieceMetadata}> Get MetaData</Button>
+              </SimpleCard>
+
+              <SimpleCard title="Get Track Info">
+                  <div>{JSON.stringify(this.display.trackInfo)}</div>
+                  <Button onClick={this.getTrackInfo}> Get Track Info </Button>
+              </SimpleCard>
+
+              <SimpleCard title="Get Track Metadata">
+                  <div>{JSON.stringify(this.display.trackMetadata)}</div>
+                  <Button onClick={this.getTrackMetadata}> Get Track Metadata </Button>
+              </SimpleCard>
+
+              <SimpleCard title="Get Track Overview">
+                  <div>{JSON.stringify(this.display.trackOverview)}</div>
+                  <Button onClick={this.getTrackOverview}> Get Track Overview </Button>
+              </SimpleCard>
+
           </div>
+
+
       );
   };
-  //   return (
-  //     <Fragment>
-  //       <div className="pb-24 pt-7 px-5 bg-primary">
-  //         <div className="card-title capitalize text-white mb-1 text-white-secondary">
-  //             New Midi Project
-  //         </div>
-  //       </div>
-  //         <SimpleCard>
-  //             <div className="card-title capitalize text-white mb-1 text-primary">
-  //               Upload Midi File
-  //           </div>
-  //             <br/>
-  //
-  //         </SimpleCard>
-  //     </Fragment>
-  //   );
-  // }
 
 
 
