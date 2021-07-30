d6f1acba7499c49c76841273dd87fcec6fd338a6
Claudio-Uni <73423543+Claudio-Uni@users.noreply.github.com>
2021-07-30 12:04:15 +0200

    adding framework for intelligence subsystem

--- backend/src/main/java/com/noxception/midisense/intelligence/IntelligenceService.java
+++ backend/src/main/java/com/noxception/midisense/intelligence/IntelligenceService.java
@@ -1,4 +1,10 @@
 package com.noxception.midisense.intelligence;
 
+import com.noxception.midisense.intelligence.rrobjects.AnalyseGenreRequest;
+import com.noxception.midisense.intelligence.rrobjects.AnalyseGenreResponse;
+
 public interface IntelligenceService {
+
+    public AnalyseGenreResponse analyseGenre(AnalyseGenreRequest req);
+
 }
--- backend/src/main/java/com/noxception/midisense/intelligence/IntelligenceServiceImpl.java
+++ backend/src/main/java/com/noxception/midisense/intelligence/IntelligenceServiceImpl.java
@@ -2,6 +2,11 @@ package com.noxception.midisense.intelligence;
 
 import org.springframework.stereotype.Service;
 
+
 @Service
 public class IntelligenceServiceImpl implements IntelligenceService{
+
+
+
+
 }
