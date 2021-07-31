0a924b44b711dffa82e43da99da51b10d02a7f20
Adrian-Rae-19004029 <u19004029@tuks.co.za>
2021-07-31 23:57:02 +0200

    Added comments for score entities and repositories

--- backend/src/main/java/com/noxception/midisense/interpreter/repository/ScoreRepository.java
+++ backend/src/main/java/com/noxception/midisense/interpreter/repository/ScoreRepository.java
@@ -6,7 +6,17 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.Optional;
 import java.util.UUID;
 
+/**
+ * An interface for defining interaction with score entities and an external CRUD repository
+ */
 public interface ScoreRepository extends CrudRepository<ScoreEntity, Long> {
+
+    /** A lookup function for existing scores in the database
+     *
+     * @param fileDesignator the unique identifier which distinguishes interpreted works in the repository
+     * @return a collection of scores whose file designator is matched completely. Returns none if there is no such score
+     * in the repository
+     */
     @Query("SELECT s FROM ScoreEntity s WHERE s.fileDesignator=?1")
     Optional<ScoreEntity> findByFileDesignator(String fileDesignator);
 }
