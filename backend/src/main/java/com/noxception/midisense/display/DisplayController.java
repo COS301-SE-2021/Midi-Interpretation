2498394a9d4c0ba090ade000310379e14db7ee1d
Adrian-Rae-19004029 <u19004029@tuks.co.za>
2021-07-31 09:56:48 +0200

    Removing extraneous Display Subsystem elements

--- backend/src/main/java/com/noxception/midisense/display/DisplayController.java
+++ backend/src/main/java/com/noxception/midisense/display/DisplayController.java
@@ -11,7 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.UUID;
 
