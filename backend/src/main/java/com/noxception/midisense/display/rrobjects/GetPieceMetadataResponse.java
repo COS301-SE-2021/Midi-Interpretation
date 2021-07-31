99bc30438a530aff11b9e418d539aecad31ad2e1
u18094024 <u18094024@tuks.co.za>
2021-07-31 22:50:24 +0200

    added comments for GetPieceMetaResponse.java, GetTrackInfoRequest  and Response, and GetTrackMetadataRequest

--- backend/src/main/java/com/noxception/midisense/display/rrobjects/GetPieceMetadataResponse.java
+++ backend/src/main/java/com/noxception/midisense/display/rrobjects/GetPieceMetadataResponse.java
@@ -6,24 +6,45 @@ import com.noxception.midisense.interpreter.dataclass.TempoIndication;
 import com.noxception.midisense.interpreter.dataclass.TimeSignature;
 
 public class GetPieceMetadataResponse extends ResponseObject {
+    /**
+     * ATTRIBUTES
+     */
     private final KeySignature keySignature;
     private  final TimeSignature timeSignature;
     private final TempoIndication tempoIndication;
 
+    /**
+     * CONSTRUCTOR
+     * @param keySignature key signature of the piece
+     * @param timeSignature time signature of the piece
+     * @param tempoIndication tempo indication of the piece
+     */
     public GetPieceMetadataResponse(KeySignature keySignature, TimeSignature timeSignature, TempoIndication tempoIndication) {
         this.keySignature = keySignature;
         this.timeSignature = timeSignature;
         this.tempoIndication = tempoIndication;
     }
 
+    /**
+     * GET method
+     * @return keySignature of the piece
+     */
     public KeySignature getKeySignature() {
         return keySignature;
     }
 
+    /**
+     * GET method
+     * @return timeSignature of the piece
+     */
     public TimeSignature getTimeSignature() {
         return timeSignature;
     }
 
+    /**
+     * GET method
+     * @return tempoIndication of the piece
+     */
     public TempoIndication getTempoIndication() {
         return tempoIndication;
     }
--- backend/src/main/java/com/noxception/midisense/display/rrobjects/GetTrackInfoRequest.java
+++ backend/src/main/java/com/noxception/midisense/display/rrobjects/GetTrackInfoRequest.java
@@ -5,12 +5,23 @@ import com.noxception.midisense.config.dataclass.RequestObject;
 import java.util.UUID;
 
 public class GetTrackInfoRequest extends RequestObject {
+    /**
+     * ATTRIBUTE
+     */
     private final UUID fileDesignator;
 
+    /**
+     * CONSTRUCTOR
+     * @param fileDesignator used to identify the file
+     */
     public GetTrackInfoRequest(UUID fileDesignator) {
         this.fileDesignator = fileDesignator;
     }
 
+    /**
+     * GET Method
+     * @return fileDesignator
+     */
     public UUID getFileDesignator() {
         return fileDesignator;
     }
