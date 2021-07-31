3f95cafd19489d138501c32c8b55af8ac2ced1e4
u18094024 <u18094024@tuks.co.za>
2021-07-31 23:09:25 +0200

    added comments for GetTrackMetadataResponse.java, GetTrackOverviewRequest.java and GetTrackOverviewResponse.java

--- backend/src/main/java/com/noxception/midisense/display/rrobjects/GetTrackMetadataResponse.java
+++ backend/src/main/java/com/noxception/midisense/display/rrobjects/GetTrackMetadataResponse.java
@@ -4,12 +4,23 @@ import com.noxception.midisense.config.dataclass.ResponseObject;
 
 public class GetTrackMetadataResponse extends ResponseObject {
     //TODO: update to be a more comprehensive class once settled on structure for track
+    /**
+     * ATTRIBUTE
+     */
     private final String trackString;
 
+    /**
+     * CONSTRUCTOR
+     * @param trackString
+     */
     public GetTrackMetadataResponse(String trackString) {
         this.trackString = trackString;
     }
 
+    /**
+     * GET method
+     * @return trackString
+     */
     public String getTrackString() {
         return trackString;
     }
