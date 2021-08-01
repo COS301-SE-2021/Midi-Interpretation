aeb591794d39dafc424f75d7f9a3f5d52ebc73a4
Rea <70331859+u18179802@users.noreply.github.com>
2021-08-01 18:17:59 +0200

    Added analyseGenre rrobjects

---
+++ backend/src/main/java/com/noxception/midisense/intelligence/rrobjects/AnalyseGenreResponse.java
@@ -0,0 +1,28 @@
+package com.noxception.midisense.intelligence.rrobjects;
+
+import com.noxception.midisense.config.dataclass.ResponseObject;
+
+import java.util.List;
+
+public class AnalyseGenreResponse extends ResponseObject {
+    /**
+     * ATTRIBUTE
+     */
+    private final List<String> genreArray;
+
+    /**
+     * CONSTRUCTOR
+     * @param genreArray an array consisting of the most prevelant genres in the piece
+     */
+    public AnalyseGenreResponse(List<String> genreArray) {
+        this.genreArray = genreArray;
+    }
+
+    /**
+     * GET method
+     * @return genreArray
+     */
+    public List<String> getGenreArray() {
+        return genreArray;
+    }
+}
