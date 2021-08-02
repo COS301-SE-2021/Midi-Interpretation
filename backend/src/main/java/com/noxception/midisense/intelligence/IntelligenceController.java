b1406d629f2cad6d45728510be09ff708c28d6da
Rea <70331859+u18179802@users.noreply.github.com>
2021-08-02 21:12:19 +0200

    Intelligence controller

--- backend/src/main/java/com/noxception/midisense/intelligence/IntelligenceController.java
+++ backend/src/main/java/com/noxception/midisense/intelligence/IntelligenceController.java
@@ -1,10 +1,42 @@
 package com.noxception.midisense.intelligence;
 
-import com.noxception.midisense.config.MIDISenseConfig;
+import com.noxception.midisense.api.IntelligenceApi;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
+import com.noxception.midisense.display.exceptions.InvalidTrackException;
+import com.noxception.midisense.intelligence.rrobjects.*;
+import com.noxception.midisense.interpreter.exceptions.InvalidDesignatorException;
+import com.noxception.midisense.models.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.UUID;
 
 @CrossOrigin("*")
 @RestController
-public class IntelligenceController {
+public class IntelligenceController implements IntelligenceApi {
+
+    @Autowired
+    IntelligenceServiceImpl intelligenceService;
+
+
+    @Override
+    public ResponseEntity<IntelligenceAnalyseGenreResponse> intelligenceAnalyseGenrePost(IntelligenceAnalyseGenreRequest body) {
+        IntelligenceAnalyseGenreResponse responseObject = new IntelligenceAnalyseGenreResponse();
+        HttpStatus returnStatus = HttpStatus.OK;
+        try{
+            UUID fileDesignator = UUID.fromString(body.getFileDesignator());
+            //========================
+            AnalyseGenreRequest req = new AnalyseGenreRequest(fileDesignator);
+            AnalyseGenreResponse res = intelligenceService.analyseGenre(req);
+            //=======================
+            responseObject.addAll(res.getGenreArray());
+        }
+        catch(InvalidDesignatorException | IllegalArgumentException e){
+            returnStatus = HttpStatus.BAD_REQUEST;
+            responseObject = null;
+        }
+        return new ResponseEntity<>(responseObject,returnStatus);
+    }
 }
