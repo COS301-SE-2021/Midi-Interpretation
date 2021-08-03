f7f11b94b93e671627a606455f404f3d2d6a6379
u18094024 <u18094024@tuks.co.za>
2021-08-03 12:40:30 +0200

    added comments for MissingStrategyException.java and NeuralNetworkGenreAnalysisStrategy.java

--- backend/src/main/java/com/noxception/midisense/intelligence/exceptions/MissingStrategyException.java
+++ backend/src/main/java/com/noxception/midisense/intelligence/exceptions/MissingStrategyException.java
@@ -1,6 +1,10 @@
 package com.noxception.midisense.intelligence.exceptions;
 
 public class MissingStrategyException extends Exception{
+    /**
+     * Exception method for identifying missing strategies
+     * @param message
+     */
     public MissingStrategyException(String message) {
         super(message);
     }
--- backend/src/main/java/com/noxception/midisense/intelligence/strategies/NeuralNetworkGenreAnalysisStrategy.java
+++ backend/src/main/java/com/noxception/midisense/intelligence/strategies/NeuralNetworkGenreAnalysisStrategy.java
@@ -3,6 +3,11 @@ package com.noxception.midisense.intelligence.strategies;
 import com.noxception.midisense.intelligence.dataclass.GenrePredication;
 
 public class NeuralNetworkGenreAnalysisStrategy implements GenreAnalysisStrategy{
+    /**
+     * Method to classify the most likely genres to be associated with a piece
+     * @param features placeholder
+     * @return an array of predicted genres for the piece
+     */
     @Override
     public GenrePredication[] classify(byte[] features) {
         return new GenrePredication[0];
