41765d5124621cbec4ec5d16d1811dec61a6e0da
Adrian-Rae-19004029 <u19004029@tuks.co.za>
2021-08-04 10:43:41 +0200

    Readability update for integration tests

--- backend/src/test/java/com/noxception/midisense/controller/InterpreterControllerIntegrationTest.java
+++ backend/src/test/java/com/noxception/midisense/controller/InterpreterControllerIntegrationTest.java
@@ -34,63 +34,15 @@ class InterpreterControllerIntegrationTest extends MidiSenseIntegrationTest{
     @Autowired
     private MockMvc mvc;
 
-    @Transactional
-    @Rollback(value = true)
-    @Test
-    void test_UploadFile_IfValidFileContents_ThenPresentDesignator() throws Exception {
-
-        byte[] validFile = new byte[]{1,2,3,4,5};
-
-        InterpreterUploadFileRequest request = new InterpreterUploadFileRequest();
-
-        //Generate a list of integer that corresponds to valid byte stream.
-        List<Integer> fileContents = new ArrayList<>();
-        for(byte b: validFile)
-            fileContents.add((int) b);
-
-        request.setFileContents(fileContents);
-        MvcResult result = mockRequest("interpreter","uploadFile",request, mvc);
-
-        //Successful request
-        Assertions.assertEquals(200,result.getResponse().getStatus());
-
-        //TODO: Want a designator
-        List<Integer> contents = request.getFileContents();
-        StringBuilder des = new StringBuilder();
-        Iterator<Integer> i = contents.iterator();
-        while(i.hasNext()){
-            des.append(i.next());
-        }
-        String designator = des.toString();
-
-        //Check to see whether is valid
-        UUID fileDesignator = UUID.fromString(designator);
-
-        //check that the resultant file can be opened : was saved to the right directory
-        String filename = MIDISenseConfig.configuration(MIDISenseConfig.ConfigurationName.MIDI_STORAGE_ROOT)
-                +fileDesignator
-                +MIDISenseConfig.configuration(MIDISenseConfig.ConfigurationName.FILE_FORMAT);
-        File newlyCreated = new File(filename);
-
-        //check to see that the file contents are the same
-        byte[] newContents = Files.readAllBytes(newlyCreated.toPath());
-        assertArrayEquals(newContents,validFile);
-
-        newlyCreated = new File(filename);
-        //delete the file
-        assertTrue(newlyCreated.delete());
-
-    }
-
+    //TODO: Implement new framework
 
     @Ignore
     @Test
     @DisplayName("Tests uploading a valid file")
     void testUploadFileValidFile() throws Exception{
-        //TODO: USE SWAGGER UI FOR NOW - FIGURE OUT WHY HTTP 415
         InterpreterUploadFileRequest request = new InterpreterUploadFileRequest();
         request.setFileContents(Arrays.asList(1, 2, 3));
-        MvcResult response = TestingDictionary.mockRequest("interpreter","uploadFile",request, mvc);
+        MvcResult response = mockRequest("interpreter","uploadFile",request, mvc);
         Assertions.assertEquals(415, response.getResponse().getStatus());
     }
 
@@ -100,10 +52,10 @@ class InterpreterControllerIntegrationTest extends MidiSenseIntegrationTest{
     void testUploadFileInvalidFile() throws Exception{
         InterpreterUploadFileRequest request = new InterpreterUploadFileRequest();
         List<Integer> newByteArray = new ArrayList<>();
-        byte[] inArray = TestingDictionary.interpreter_uploadFile_invalidFileContents;
+        byte[] inArray = new byte[]{};
         if(inArray != null) for (byte b : inArray) newByteArray.add((int) b);
         request.setFileContents(newByteArray);
-        MvcResult response = TestingDictionary.mockRequest("interpreter","uploadFile",request, mvc);
+        MvcResult response = mockRequest("interpreter","uploadFile",request, mvc);
         Assertions.assertEquals(415, response.getResponse().getStatus());
     }
 
@@ -113,8 +65,9 @@ class InterpreterControllerIntegrationTest extends MidiSenseIntegrationTest{
     @DisplayName("Tests processing a valid file")
     void testProcessFileValidFileDesignator() throws Exception{
         InterpreterProcessFileRequest request = new InterpreterProcessFileRequest();
+
         request.setFileDesignator(TestingDictionary.interpreter_all_validFileDesignator);
-        MvcResult response = TestingDictionary.mockRequest("interpreter","processFile",request, mvc);
+        MvcResult response = mockRequest("interpreter","processFile",request, mvc);
         Assertions.assertEquals(200, response.getResponse().getStatus());
     }
 
@@ -125,7 +78,7 @@ class InterpreterControllerIntegrationTest extends MidiSenseIntegrationTest{
     void testProcessFileInvalidFileDesignator() throws Exception{
         InterpreterProcessFileRequest request = new InterpreterProcessFileRequest();
         request.setFileDesignator(TestingDictionary.interpreter_all_invalidFileDesignator);
-        MvcResult response = TestingDictionary.mockRequest("interpreter","processFile",request, mvc);
+        MvcResult response = mockRequest("interpreter","processFile",request, mvc);
         Assertions.assertEquals(200, response.getResponse().getStatus());
     }
 
