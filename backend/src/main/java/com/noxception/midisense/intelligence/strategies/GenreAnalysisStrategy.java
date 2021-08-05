357d3efb5e7ab939653e48a12c9baa2116fde18a
Adrian-Rae-19004029 <u19004029@tuks.co.za>
2021-08-05 19:28:43 +0200

    Added more classifier framework methods and a linear algebra framework

--- backend/src/main/java/com/noxception/midisense/intelligence/strategies/GenreAnalysisStrategy.java
+++ backend/src/main/java/com/noxception/midisense/intelligence/strategies/GenreAnalysisStrategy.java
@@ -4,5 +4,8 @@ import com.noxception.midisense.intelligence.dataclass.GenrePredication;
 
 public interface GenreAnalysisStrategy {
 
+    int totalSuggestions = 10;
+    String[] classes = {"rock", "classic rock", "album rock", "hard rock", "permanent wave", "mellow gold", "alternative metal", "soft rock", "alternative rock", "metal", "pop rock", "art rock", "nu metal", "dance pop", "country rock", "folk rock", "new wave pop", "country", "pop", "europop", "adult standards", "punk", "progressive rock", "symphonic rock", "blues rock", "modern rock", "new wave", "psychedelic rock", "post-grunge", "glam rock", "funk metal", "rap metal", "new romantic", "power metal", "folk", "neo classical metal", "synthpop", "singer-songwriter", "progressive metal", "skate punk", "rap rock", "pop punk", "country road", "brill building pop", "dance rock", "post-teen pop", "grunge", "roots rock", "british invasion", "beatlesque", "thrash metal", "glam metal", "contemporary country", "socal pop punk", "old school thrash", "funk rock", "zolo", "lounge", "soul", "piano rock", "heartland rock", "symphonic metal", "rock-and-roll", "yacht rock", "canadian pop", "urban contemporary", "rockabilly", "jazz fusion", "instrumental rock", "neo mellow", "speed metal", "industrial metal", "bubblegum pop", "gothic metal", "easy listening", "pop rap", "death metal", "merseybeat", "hip hop", "art pop", "motown", "nashville sound", "disco", "quiet storm", "lilith", "country dawn", "melodic metal", "melancholia", "rap", "r&b", "german metal", "funk", "classic canadian rock", "ska punk", "britpop", "finnish metal", "classic country pop", "industrial rock", "groove metal","vocal jazz"};
+
     public GenrePredication[] classify(byte[] features);
 }
--- backend/src/main/java/com/noxception/midisense/intelligence/strategies/NeuralNetworkGenreAnalysisStrategy.java
+++ backend/src/main/java/com/noxception/midisense/intelligence/strategies/NeuralNetworkGenreAnalysisStrategy.java
@@ -1,8 +1,48 @@
 package com.noxception.midisense.intelligence.strategies;
 
 import com.noxception.midisense.intelligence.dataclass.GenrePredication;
+import org.ejml.simple.SimpleMatrix;
+
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class NeuralNetworkGenreAnalysisStrategy implements GenreAnalysisStrategy{
+
+    private final int inputLayerSize = 100;
+    private final int hiddenLayerSize = 100;
+    private final int classificationClasses = 100;
+
+    // Network Weights
+    private SimpleMatrix w1;
+    private SimpleMatrix w2;
+
+    // Network Biases
+    private SimpleMatrix b1;
+    private SimpleMatrix b2;
+
+
+
+
+    public NeuralNetworkGenreAnalysisStrategy() {
+
+        //allocate weights and biases
+        w1 = new SimpleMatrix(hiddenLayerSize,inputLayerSize);
+        b1 = new SimpleMatrix(hiddenLayerSize,1);
+
+        w2 = new SimpleMatrix(classificationClasses,hiddenLayerSize);
+        b2 = new SimpleMatrix(classificationClasses,1);
+
+        //randomise : TODO: replace by loading generated values
+        for(SimpleMatrix caseMatrix: new SimpleMatrix[]{w1, w2, b1, b2}){
+            for(int i=0; i<caseMatrix.numRows(); i++){
+                for(int j=0; j<caseMatrix.numCols(); j++){
+                    caseMatrix.set(i,j,Math.random()/100);
+                }
+            }
+        }
+
+    }
+
     /**
      * Method to classify the most likely genres to be associated with a piece
      * @param features placeholder
@@ -10,11 +50,99 @@ public class NeuralNetworkGenreAnalysisStrategy implements GenreAnalysisStrategy
      */
     @Override
     public GenrePredication[] classify(byte[] features) {
-        // TODO: replace mock with working classifier
-        return new GenrePredication[]{
-                new GenrePredication("Rock",0.98),
-                new GenrePredication("Pop",0.92),
-                new GenrePredication("Ska",0.56)
-        };
+
+        //forward propagation
+        SimpleMatrix inputLayer = new SimpleMatrix(inputLayerSize,1);
+
+        //set features : TODO fill in the necessary features from the array here
+
+        //feed forward
+        SimpleMatrix outputLayer = feedForward(inputLayer);
+
+        //get the predictions for the classes
+        GenrePredication[] totalPredictions = new GenrePredication[classificationClasses];
+        for(int i=0; i<classificationClasses; i++){
+            totalPredictions[i] = new GenrePredication(classes[i],outputLayer.get(i,0));
+        }
+
+        //sort the predictions
+        Arrays.sort(totalPredictions,new sortByConfidence());
+
+        //return only the specified amount
+        return Arrays.copyOfRange(totalPredictions,0,totalSuggestions);
+
     }
+
+    public SimpleMatrix feedForward(SimpleMatrix x){
+
+        //layer 1 activation
+        SimpleMatrix z1 = (w1.mult(x)).plus(b1);
+        SimpleMatrix a1 = tanh(z1);
+
+        //layer 2 activation
+        SimpleMatrix z2 = (w2.mult(a1)).plus(b2);
+        SimpleMatrix a2 = sigmoid(z2);
+
+        return a2;
+    }
+
+    public int getInputLayerSize() {
+        return inputLayerSize;
+    }
+
+    public int getHiddenLayerSize() {
+        return hiddenLayerSize;
+    }
+
+    public int getClassificationClasses() {
+        return classificationClasses;
+    }
+
+    public int getTotalSuggestions() {
+        return totalSuggestions;
+    }
+
+    // Element-Wise Activation
+
+    private double sigmoid(double x){
+        return 1.0 / (1.0 + Math.exp(-1*x));
+    }
+
+    private double tanh(double x){
+        return Math.tanh(x);
+    }
+
+    // Matrix-wise Activations
+
+    public SimpleMatrix sigmoid(SimpleMatrix A){
+        SimpleMatrix B = new SimpleMatrix(A);
+        for (int i=0; i<B.numRows(); i++){
+            for (int j=0; j<B.numCols(); j++){
+                B.set(i,j,sigmoid(B.get(i,j)));
+            }
+        }
+        return B;
+    }
+
+    public SimpleMatrix tanh(SimpleMatrix A){
+        SimpleMatrix B = new SimpleMatrix(A);
+        for (int i=0; i<B.numRows(); i++){
+            for (int j=0; j<B.numCols(); j++){
+                B.set(i,j,tanh(B.get(i,j)));
+            }
+        }
+        return B;
+    }
+
+    static class sortByConfidence implements Comparator<GenrePredication>{
+
+        @Override
+        public int compare(GenrePredication o1, GenrePredication o2) {
+            return (int) Math.round(o1.getCertainty()-o2.getCertainty());
+        }
+    }
+
+
+
+
 }
