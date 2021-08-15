b47f9b08cbb87ff8cd343bcbd5562c49325715b0
Rea <70331859+u18179802@users.noreply.github.com>
2021-08-15 20:23:26 +0200

    Intelligence integration test skeleton

--- backend/src/test/java/com/noxception/midisense/controller/IntelligenceServiceIT.java
+++ backend/src/test/java/com/noxception/midisense/controller/IntelligenceServiceIT.java
@@ -1,4 +1,19 @@
 package com.noxception.midisense.controller;
 
+import com.noxception.midisense.config.ConfigurationName;
+import com.noxception.midisense.config.MIDISenseConfig;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import com.noxception.midisense.models.IntelligenceAnalyseGenreRequest;
+import org.junit.jupiter.api.DisplayName;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class IntelligenceServiceIT extends MidiSenseIntegrationTest{
 }
