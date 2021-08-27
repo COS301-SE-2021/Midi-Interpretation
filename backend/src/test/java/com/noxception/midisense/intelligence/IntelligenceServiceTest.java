201c7b6501c16503b5f2f7e0c06cf0e50d2a205c
Claudio-Uni <73423543+Claudio-Uni@users.noreply.github.com>
2021-08-27 11:09:54 +0200

    Testing for the AnalyseChord being implemented

--- backend/src/test/java/com/noxception/midisense/intelligence/IntelligenceServiceTest.java
+++ backend/src/test/java/com/noxception/midisense/intelligence/IntelligenceServiceTest.java
@@ -480,6 +480,50 @@ public class IntelligenceServiceTest extends MIDISenseUnitTest {
         }
     }
 
+    @Test
+    public void testWhiteBox_AnalyseChord_IfAltered_ThenAdheresToIntervals() throws MissingStrategyException {
+
+        //For each pitch offset
+        for(int k=0; k<12; k++){
+
+
+            byte[][] testingCases = new byte[][]{
+                    //these need to change a bit
+                    new byte[]{(byte) (0+k),(byte) (6+k), (byte) (6+k), (byte) (9+k)},
+                    new byte[]{(byte) (60+k),(byte) (72+k), (byte) (67+k), (byte) (69+k)},
+                    new byte[]{(byte) (0+k),(byte) (3+k),(byte) (17+k),(byte) (9+k),(byte) (12+k), (byte) (15+k),(byte) (18+k), (byte) (21+k)},
+                    new byte[]{(byte) (3+k),(byte) (6+k), (byte) (17+k), (byte) (12+k)},
+                    new byte[]{(byte) (6+k),(byte) (9+k),(byte) (12+k), (byte) (15+k)},
+                    new byte[]{(byte) (9+k),(byte) (17+k),(byte) (18+k), (byte) (18+k)},
+            };
+
+
+            ChordPrediction[] testingResponses = new ChordPrediction[]{
+                    new ChordPrediction((byte) (0+k),(byte) (0+k), ChordType.ALTERED),
+                    new ChordPrediction((byte) (0+k),(byte) (0+k), ChordType.ALTERED),
+                    new ChordPrediction((byte) (0+k),(byte) (0+k), ChordType.ALTERED),
+                    new ChordPrediction((byte) (0+k),(byte) (3+k), ChordType.ALTERED),
+                    new ChordPrediction((byte) (0+k),(byte) (6+k), ChordType.ALTERED),
+                    new ChordPrediction((byte) (0+k),(byte) (9+k), ChordType.ALTERED),
+            };
+
+
+            //Testing all cases with their expected responses
+            for(int j=0; j<testingCases.length; j++){
+
+                AnalyseChordRequest request = new AnalyseChordRequest(testingCases[j]);
+                AnalyseChordResponse response = intelligenceService.analyseChord(request);
+
+                String responseChord = response.getChord();
+                String expectedChord = testingResponses[j].getCommonName();
+                System.out.printf("TESTING PITCH OFFSET: ", k);
+                System.out.printf("EXPECTED %s GOT %s%n",expectedChord,responseChord);
+                assertEquals(responseChord,expectedChord);
+            }
+
+        }
+    }
+
 
 
 }
