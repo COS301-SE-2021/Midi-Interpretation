d4afa70f0dec771226210fc8ef4e2020861d39dd
Rea <70331859+u18179802@users.noreply.github.com>
2021-09-08 16:11:37 +0200

    updated upload file whitebox test

--- backend/src/test/java/com/noxception/midisense/interpreter/InterpreterServiceTest.java
+++ backend/src/test/java/com/noxception/midisense/interpreter/InterpreterServiceTest.java
@@ -13,6 +13,7 @@ import com.noxception.midisense.interpreter.rrobjects.ParseJSONResponse;
 import com.noxception.midisense.interpreter.rrobjects.UploadFileRequest;
 import com.noxception.midisense.interpreter.rrobjects.UploadFileResponse;
 import com.noxception.midisense.repository.DatabaseManager;
+import com.noxception.midisense.repository.ScoreEntity;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -650,50 +651,67 @@ class InterpreterServiceTest extends MIDISenseUnitTest {
 //    //                                  WHITE BOX TESTING BELOW                                                           //
 //    //====================================================================================================================//
 //
-//    /**UploadFile*/
-//    /**
-//     * Description: tests the uploadFile() function by passing in a valid file and saving
-//     * to the right directory
-//     * precondition - valid byte stream passed in
-//     * post condition - valid UUID from the right directory with the sames contents
-//     */
-//    @Test
-//    @DisplayName("Uploading File: input [valid byte stream] expect [valid UUID corresponding to file with same contents]")
-//    public void testWhiteBox_UploadFile_IfValidFile_ThenAccurateInfo() throws InvalidUploadException, IOException {
-//
-//        // Generate Valid File, put it in request
-//        byte[] validFile = new byte[]{1, 2, 3, 4, 5};
-//        UploadFileRequest req = new UploadFileRequest(validFile);
-//
-//        // Upload the file and get the designator
-//        UploadFileResponse res = interpreterService.uploadFile(req);
-//        UUID fileDesignator = res.getFileDesignator();
-//
-//        //check the designator is not null
-//        assertNotNull(fileDesignator);
-//
-//        ScoreEntity testEntity = new ScoreEntity();
-//        testEntity.setFileDesignator(fileDesignator.toString());
-//        testEntity.setKeySignature("Cbmaj");
-//        testEntity.setTempoIndication(70);
-//        testEntity.setTimeSignature("4/4");
-//        databaseManager.save(testEntity);
-//
-//        //check that the resultant file can be opened : was saved to the right directory
-//        String filename = configurations.configuration(ConfigurationName.MIDI_STORAGE_ROOT)
-//                + fileDesignator
-//                + configurations.configuration(ConfigurationName.FILE_FORMAT);
-//        File newlyCreated = new File(filename);
-//
-//        //check to see that the file contents are the same
-//        byte[] newContents = Files.readAllBytes(newlyCreated.toPath());
-//        assertArrayEquals(newContents, validFile);
-//
-//        newlyCreated = new File(filename);
-//        //delete the file
-//        assertTrue(newlyCreated.delete());
-//    }
-//
+    /**UploadFile*/
+    /**
+     * Description: tests the uploadFile() function by passing in a valid file and saving
+     * to the right directory
+     * precondition - valid byte stream passed in
+     * post condition - valid UUID from the right directory with the sames contents
+     */
+    @Test
+    @DisplayName("Uploading File: input [valid byte stream] expect [valid UUID corresponding to file with same contents]")
+    public void testWhiteBox_UploadFile_IfValidFile_ThenAccurateInfo() throws InvalidUploadException, IOException {
+
+        // Generate Valid File, put it in request
+        byte[] validFile = new byte[]{1, 2, 3, 4, 5};
+        UploadFileRequest req = new UploadFileRequest(validFile);
+
+        // Upload the file and get the designator
+        UploadFileResponse res = interpreterService.uploadFile(req);
+        UUID fileDesignator = res.getFileDesignator();
+
+        //check the designator is not null
+        assertNotNull(fileDesignator);
+
+        ScoreEntity testEntity = new ScoreEntity();
+        Score s = new Score();
+        testEntity.setFileDesignator(fileDesignator.toString());
+        KeySignature keySignature = new KeySignature();
+        keySignature.tick =0;
+        keySignature.commonName = "Cb";
+        s.KeySignatureMap.add(keySignature);
+
+        TempoIndication tempoIndication = new TempoIndication();
+        tempoIndication.tick=0;
+        tempoIndication.tempoIndication = 70;
+        s.TempoIndicationMap.add(tempoIndication);
+
+        TimeSignature timeSignature = new TimeSignature();
+        TimeSignature.InnerTime innerTime = new TimeSignature.InnerTime();
+        innerTime.beatValue =4;
+        innerTime.numBeats=4;
+        timeSignature.tick=0;
+        timeSignature.time= innerTime;
+        s.TimeSignatureMap.add(timeSignature);
+
+        testEntity.encodeScore(s);
+        databaseManager.save(testEntity);
+
+        //check that the resultant file can be opened : was saved to the right directory
+        String filename = configurations.configuration(ConfigurationName.MIDI_STORAGE_ROOT)
+                + fileDesignator
+                + configurations.configuration(ConfigurationName.FILE_FORMAT);
+        File newlyCreated = new File(filename);
+
+        //check to see that the file contents are the same
+        byte[] newContents = Files.readAllBytes(newlyCreated.toPath());
+        assertArrayEquals(newContents, validFile);
+
+        newlyCreated = new File(filename);
+        //delete the file
+        assertTrue(newlyCreated.delete());
+    }
+
 //    /**
 //     * Description: tests the processFile() function by passing in a midi file designator that
 //     * exists in storage
